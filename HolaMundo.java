e58b09c98d3a7e58a5ee28b15dd683fc4e90f8c7
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2022-09-15 19:06:33 -0400

    Nuevo Repositorio Hola Mundo Java

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        int r;
+       r = suma(50, 25, 25);
+        System.out.println(r);
+    }
+    public static int suma (int a, int b, int c) {
+        return a + b + c;
+    }
+}
